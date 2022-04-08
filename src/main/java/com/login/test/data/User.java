06dbc7084629e3920d5b5bebdc49cdb914f8a228
William Arias <willi.am@hotmail.es>
2022-04-08 12:57:34 -0600

    Base classes and packages created.

--- src/main/java/com/login/test/data/User.java
+++ src/main/java/com/login/test/data/User.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.login.test;
+
+package com.login.test.data;
 
 /**
  *
  * @author willi
  */
+
 public class User {
     
     private String username;
