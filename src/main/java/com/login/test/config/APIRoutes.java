6d658b958c02a65fb806f18ac36c104cd4bc6960
William Arias <willi.am@hotmail.es>
2022-04-11 01:54:36 -0600

    -More classes and constant variables added to APIRoutes. -Added more dependencies and fixed versions error. -User Controller implemented with getAll() method. -Postman reaches project API and getAll() works properly.

--- src/main/java/com/login/test/config/APIRoutes.java
+++ src/main/java/com/login/test/config/APIRoutes.java
@@ -6,7 +6,21 @@ package com.login.test.config;
  */
 public class APIRoutes {
 
+    public class Session {
+
+        public static final String LOG_IN = "/login";
+        public static final String LOG_OUT = "/logout";
+    }
+
     public class RestAPI {
+
+        public static final String GET_ALL = "/getAll";
+        public static final String GET_BY_ID = "/{id}";
+
+    }
+
+    public class API {
+
         public static final String TYPE = "/api";
         public static final String VERSION_1 = "/v1";
         public static final String USERS_V1 = TYPE + VERSION_1 + "/user";
--- src/main/java/com/login/test/controller/UserController.java
+++ src/main/java/com/login/test/controller/UserController.java
@@ -1,25 +1,39 @@
 package com.login.test.controller;
 
 import com.login.test.config.APIRoutes;
+import com.login.test.dto.UserDto;
 import com.login.test.service.ILoginService;
 import com.login.test.service.IService;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.modelmapper.ModelMapper;
+
 /**
  *
  * @author willi
  */
-
 @RestController
-@RequestMapping(APIRoutes.RestAPI.USERS_V1)
+@RequestMapping(APIRoutes.API.USERS_V1)
 public class UserController {
-    
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Autowired
     private IService service;
-    
+
     @Autowired
     private ILoginService login;
-    
-    
+
+    @GetMapping(APIRoutes.RestAPI.GET_ALL)
+    public List<UserDto> getAllUsers() {
+
+        return (List<UserDto>) service.getAll().stream().map(user -> modelMapper
+                .map(user, UserDto.class)).collect(Collectors.toList());
+    }
 }
